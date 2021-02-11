fcb087fe5de8be6324339373129b473ac60a008c
Marcus <marcus.almqvist@hm.com>
2021-02-11 13:53:11 +0100

    "changes after PL"

---
+++ src/components/SortByButton/SortByButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import '.css';
+
+function SortByButton(
+{
+    title
+}){
+    return ( 
+            <button>
+                {title}
+            </button>
+    )
+} 
+export default SortByButton;
